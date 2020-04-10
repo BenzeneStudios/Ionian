7340fa7ea82a5360fd4acb88edbc3ff94a1a88ca
valoeghese <valoeghesemods@gmail.com>
2020-04-10 23:35:06 +1200

    totally not a licence breach

---
+++ src/main/java/io/github/ionianmc/ionian/mixin/MixinReloadableResourceManagerImpl.java
@@ -0,0 +1,10 @@
+package io.github.ionianmc.ionian.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.resource.ReloadableResourceManagerImpl;
+
+@Mixin(ReloadableResourceManagerImpl.class)
+public class MixinReloadableResourceManagerImpl {
+
+}
